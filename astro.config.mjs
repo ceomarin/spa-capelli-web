3a9f76580292f188afe96ae2ba8005aebe16d837
ceomarin <ceomarin.dev@gmail.com>
2026-06-03 22:30:17 -0400

    fix: remove sitemap plugin, causes build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://ceodevops.github.io/spa-capelli-web',
   base: '/spa-capelli-web',
-  integrations: [sitemap()],
   output: 'static',
   build: {
     assets: '_assets',
